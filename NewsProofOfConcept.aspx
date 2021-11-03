6e89e4300a2c3cab4b80b853ef60d77e8a52397f
faisalmotan <faisal.motan3@gmail.com>
2021-11-03 14:54:59 +0500

    new cform start working

---
+++ NewsProofOfConcept.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Portfolio Builder Leaderboard" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" EnableEventValidation="false" CodeBehind="NewsProofOfConcept.aspx.cs" Inherits="Unchained.Leaderboard" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Portfolio Builder Leaderboard: <small>v1.1</small></h2>
+     <asp:Button ID="btnSummary" runat="server" onclick="btnSummary_Click"  Text="Summary" />
+     <asp:Button ID="btnDetail" runat="server" onclick="btnDetail_Click"  Text="Details" />
+     <asp:Label ID="lblMode" runat="server" style="color:red;" Text=""></asp:Label></td>
+
+  <br />
+
+    <%=GetLeaderboard()%>
+
+
+
+
+</asp:Content>
---
+++ NewsProofOfConcept.aspx.cs
@@ -0,0 +1,93 @@
+﻿using static Unchained.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static BiblePayDLL.Shared;
+using static Unchained.DataOps;
+using static BiblePayCommon.Common;
+using static Unchained.BiblePayUtilities;
+using BiblePayCommonNET;
+
+namespace Unchained
+{
+    public partial class NewsProofOfConcept : BBPPage
+    {
+        protected new void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UpdateInfo()
+        {
+            string sMode = Session["leaderboardmode"].ToNonNullString() == "summary" ? "Summary" : "Details";
+            lblMode.Text = sMode;
+
+        }
+        protected string GetLeaderboard()
+        {
+
+            double nSuperblockLimit = 350000;
+
+            string html = "<table class=saved>";
+            // Column headers
+            string sRow = "<tr><th>Address<th width=10%>Nick Name<th>Currency<th>Total BBP<th>Total Foreign<th>USD Value BBP<th>USD Value Foreign<th>Assessed USD<th>Coverage<th>Earnings<th>Strength</tr>";
+            html += sRow;
+            Dictionary<string, PortfolioParticipant> u = GenerateUTXOReport(IsTestNet(this));
+            foreach (KeyValuePair<string, PortfolioParticipant> pp in u)
+            {
+                double nEarnings = nSuperblockLimit * pp.Value.Strength;
+                if (pp.Value.Strength > 0)
+                {
+                    sRow = "<tr><td>" + pp.Value.RewardAddress
+                        + "<td>" + pp.Value.NickName
+                        + "<td>Various"
+                        + "<td>" + pp.Value.AmountBBP.ToString()
+                        + "<td>" + pp.Value.AmountForeign.ToString()
+                        + "<td>" + pp.Value.AmountUSDBBP.ToString()
+                        + "<td>" + pp.Value.AmountUSDForeign.ToString()
+                        + "<td>" + pp.Value.AmountUSD.ToString()
+                        + "<td>" + Math.Round(pp.Value.Coverage * 100, 2).ToString() + "%"
+                        + "<td>" + Math.Round(nEarnings, 2).ToString()
+                        + "<td>" + Math.Round(pp.Value.Strength * 100, 2).ToString() + "%</tr>";
+                    html += sRow;
+                    if (lblMode.Text == "Details")
+                    {
+                        string sTD = "<td style='background-color:grey;'>";
+                        for (int i = 0; i < pp.Value.lPortfolios.Count; i++)
+                        {
+                            if (pp.Value.lPortfolios[i].AmountBBP > 0 || pp.Value.lPortfolios[i].AmountForeign > 0)
+                            {
+                                sRow = "<tr>" + sTD + sTD + sTD + pp.Value.lPortfolios[i].Ticker
+                                    + sTD + pp.Value.lPortfolios[i].AmountBBP.ToString()
+                                    + sTD + pp.Value.lPortfolios[i].AmountForeign.ToString()
+                                    + sTD + pp.Value.lPortfolios[i].AmountUSDBBP.ToString()
+                                    + sTD + pp.Value.lPortfolios[i].AmountUSDForeign.ToString()
+                                    + sTD + sTD + sTD + sTD;
+                                html += sRow;
+                            }
+
+                        }
+                    }
+                }
+            }
+            html += "</table>";
+            return html;
+        }
+
+        protected void btnSummary_Click(object sender, EventArgs e)
+        {
+            Session["leaderboardmode"] = "summary";
+            UpdateInfo();
+        }
+
+        protected void btnDetail_Click(object sender, EventArgs e)
+        {
+            Session["leaderboardmode"] = "detail";
+            UpdateInfo();
+        }
+    }
+}
