d965604d765708d5eed6e540a8836eb572c0b316
faisalmotan <faisal.motan3@gmail.com>
2021-11-08 17:54:22 +0500

    updates

---
+++ AddNewsFeedItem.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddNewsFeedItem.aspx.cs" Inherits="Unchained.AddNewsFeedItem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table>
+        <tr>
+            <td>
+                       <h3>Add New Ticket <small>v1.1</small></h3>
+            </td>
+        </tr>
+
+    </table>
+
+
+
+
+
+        <asp:Label ID="lblSubject" runat="server" Text="Feed Name:" ></asp:Label>
+        <br />
+        <asp:TextBox ID="txtSubject" width="400px" runat="server"></asp:TextBox>
+        <br />
+        <br />
+
+            <asp:Label ID="Label1" runat="server" Text="Ticket Body:"></asp:Label>
+    <br />
+
+        <asp:CheckBox ID="CheckBox1" runat="server" />
+      <asp:Label ID="lblBody" runat="server" Text="Ticket Body:"></asp:Label>
+        <br />
+
+        <asp:TextBox ID="txtBody" runat="server" TextMode="MultiLine"  Rows="10" class='pc90'>
+        </asp:TextBox>
+        <br />
+    <br />
+
+
+    <br />
+    <asp:Button ID="btnSave" runat="server" Text="Submit Ticket" OnClick="btnSave_Click" />
+    
+
+
+</asp:Content>
---
+++ AddNewsFeedItem.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static Unchained.Common;
+
+
+namespace Unchained
+{
+    public partial class AddNewsFeedItem : BBPPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _EntityName = "NewsFeedSource";
+
+            DataTable dt = BiblePayDLL.Sidechain.RetrieveDataTable3(IsTestNet(this), _EntityName);
+
+            string Test = "";
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+              _EntityName = "NewsFeedSource";
+
+            DataTable dt = BiblePayDLL.Sidechain.RetrieveDataTable3(IsTestNet(this), _EntityName);
+
+
+            BiblePayCommon.IBBPObject o = (BiblePayCommon.IBBPObject)BiblePayCommon.EntityCommon.GetInstance("BiblePayCommon.Entity+" + _EntityName);
+            BiblePayCommon.EntityCommon.SetEntityValue(o, "FeedName", "Test");
+            //BiblePayCommon.EntityCommon.SetEntityValue(o, "Body", txtBody.Text);
+            //BiblePayCommon.EntityCommon.SetEntityValue(o, "TicketNumber", (int)nTicketNumber);
+            BiblePayCommon.EntityCommon.SetEntityValue(o, "id", Guid.NewGuid().ToString());
+            //BiblePayCommon.EntityCommon.SetEntityValue(o, "Disposition", "");
+            //BiblePayCommon.EntityCommon.SetEntityValue(o, "AssignedTo", "");
+             BiblePayCommon.EntityCommon.SetEntityValue(o, "UserID", gUser(this).id);
+            BiblePayCommon.Common.DACResult r = DataOps.InsertIntoTable(this, IsTestNet(this), o, gUser(this));
+
+        }
+    }
+}
